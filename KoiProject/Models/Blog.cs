add95de7f7b200b8fc671819e50e3d246382ad6a
td041 <trinhtrantrungduc@gmail.com>
2024-10-17 05:10:05 +0700

    Code chính cua dự án

---
+++ KoiProject/Models/Blog.cs
@@ -0,0 +1,9 @@
+﻿public class Blog
+{
+    public int Id { get; set; }          // ID của bài viết
+    public string Title { get; set; }    // Tiêu đề bài viết
+    public string Content { get; set; }  // Nội dung bài viết
+    public string Author { get; set; }   // Tác giả bài viết
+    public DateTime PublishedDate { get; set; } // Ngày xuất bản
+    public string Category { get; set; } // Danh mục bài viết (nếu cần)
+}
---
+++ KoiProject/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } // Thêm thuộc tính ImageUrl
+}
